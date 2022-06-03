2e2fdeb90ed1e0bf45c1c073f9f033710cde48e6
Suzan97 <susannafula.sw@gmail.com>
2022-06-03 04:53:04 -0700

    alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *  
+ * main - Entry point 
  * Return: 0
  */
 
 int main(void)
 {
 	char b;
-	
 	for (b = 'a'; b <= 'z'; b++)
 		if (b != 'q' && b != 'e')
 			putchar(b);
