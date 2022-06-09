c4e5f6f85727a81915b2f6f45e3c608741a62e5b
Suzan97 <susannafula.sw@gmail.com>
2022-06-09 02:03:53 -0700

    the function positive_or_negative()

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,6 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * main - Debugging.
+ * main - causes an infinite loop
  *
  * Return: 0
  */
@@ -12,7 +11,20 @@ int main(void)
 {
 	int i;
 	
+	printf("Infinite loop incoming :(\n");
 	i = 0;
-	positive_or_negative(i);
+	
+	/**
+	 * while (i < 10)
+	 * 
+	 * {
+	 * 
+	 * putchar(i);
+	 * 
+	 * }
+	 */
+	
+	printf("Infinite loop avoided! \\o/\n");
 	return (0);
+
 }
