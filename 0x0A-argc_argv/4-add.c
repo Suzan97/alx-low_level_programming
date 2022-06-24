377f8c363b6969945ab86920151b063c5a3ed31a
Suzan97 <susannafula.sw@gmail.com>
2022-06-24 04:51:10 -0700

    program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,7 +4,7 @@
 /**
  * main - adds positive numbers.
  * @argc: number of command line arguments
- * argv: array that contains the program command line arguments
+ * @argv: array that contains the program command line arguments
  * Return: 0  - success
  */
 
